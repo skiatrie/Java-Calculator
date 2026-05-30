29305b51197fcdc03f8d3d11a811cf4b4ee4dd09
Jade Moultrie <jsmoultrie05@gmail.com>
2026-05-30 11:53:45 -0500

    this is a test for calculator

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    String c = "London";
-    int p = 9000000;
-    public static void main(String[] args) {
-        Main myObj = new Main();
-        System.out.println("City: " + myObj.c + "\nPopulation: " + myObj.p);
-    }
-}
